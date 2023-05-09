02353339e37d7e1f1412a71f2a0e9d24941c7d7a
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-09 07:21:42 -0500

    array scripts

---
+++ ArrayScripts/vars.js
@@ -0,0 +1,127 @@
+const courses = [
+  {
+    CourseId: 'PROG100',
+    Title: 'Introduction to HTML/CSS/Git',
+    Location: 'Classroom 7',
+    StartDate: '09/08/22',
+    Fee: '100.00'
+  },
+  {
+    CourseId: 'PROG200',
+    Title: 'Introduction to JavaScript',
+    Location: 'Classroom 9',
+    StartDate: '11/22/22',
+    Fee: '350.00'
+  },
+  {
+    CourseId: 'PROG300',
+    Title: 'Introduction to Java',
+    Location: 'Classroom 1',
+    StartDate: '01/09/23',
+    Fee: '50.00'
+  },
+  {
+    CourseId: 'PROG400',
+    Title: 'Introduction to SQL and Databases',
+    Location: 'Classroom 7',
+    StartDate: '03/16/23',
+    Fee: '50.00'
+  },
+  {
+    CourseId: 'PROJ500',
+    Title: 'Introduction to Angular',
+    Location: 'Classroom 1',
+    StartDate: '04/25/23',
+    Fee: '50.00'
+  }
+];
+
+const academyMembers = [
+  {
+    memID: 101,
+    name: 'Bob Brown',
+    films: ['Bob I', 'Bob II', 'Bob III', 'Bob IV']
+  },
+  {
+    memID: 142,
+    name: 'Sallie Smith',
+    films: ['A Good Day', 'A Better Day']
+  },
+  {
+    memID: 187,
+    name: 'Fred Flanders',
+    films: ['Who is Fred?', 'Where is Fred?', 'What is Fred?', 'Why Fred?']
+  },
+  {
+    memID: 203,
+    name: 'Bobbie Boots',
+    films: ['Walking Boots', 'Hiking Boots', 'Cowboy Boots']
+  }
+];
+
+const vehicles = [
+  {
+    color: 'Silver',
+    type: 'Minivan',
+    registrationState: 'CA',
+    licenseNo: 'ABC-101',
+    registrationExpires: new Date('3-10-2022'),
+    capacity: 7
+  },
+  {
+    color: 'Red',
+    type: 'Pickup Truck',
+    registrationState: 'TX',
+    licenseNo: 'A1D-2NC',
+    registrationExpires: new Date('8-31-2023'),
+    capacity: 3
+  },
+  {
+    color: 'White',
+    type: 'Pickup Truck',
+    registrationState: 'TX',
+    licenseNo: 'A22-X00',
+    registrationExpires: new Date('9-31-2023'),
+    capacity: 6
+  },
+  {
+    color: 'Red',
+    type: 'Car',
+    registrationState: 'CA',
+    licenseNo: 'ABC-222',
+    registrationExpires: new Date('12-10-2022'),
+    capacity: 5
+  },
+  {
+    color: 'Black',
+    type: 'SUV',
+    registrationState: 'CA',
+    licenseNo: 'EEE-222',
+    registrationExpires: new Date('12-10-2021'),
+    capacity: 7
+  },
+  {
+    color: 'Red',
+    type: 'SUV',
+    registrationState: 'TX',
+    licenseNo: 'ZZ2-101',
+    registrationExpires: new Date('12-30-2022'),
+    capacity: 5
+  },
+  {
+    color: 'White',
+    type: 'Pickup Truck',
+    registrationState: 'TX',
+    licenseNo: 'CAC-7YT',
+    registrationExpires: new Date('1-31-2023'),
+    capacity: 5
+  },
+  {
+    color: 'White',
+    type: 'Pickup Truck',
+    registrationState: 'CA',
+    licenseNo: '123-ABC',
+    registrationExpires: new Date('3-31-2023'),
+    capacity: 5
+  }
+];
