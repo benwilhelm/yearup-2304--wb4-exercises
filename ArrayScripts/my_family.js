b8eb5dede0df821e86ffc877798444f4c3f248ab
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-09 08:03:40 -0500

    my_family

---
+++ ArrayScripts/my_family.js
@@ -0,0 +1,12 @@
+const myFamily = ['Ben', 'Jen', 'Leif', 'Oliver'];
+
+for (let i = 0; i < myFamily.length; i++) {
+  console.log(myFamily[i]);
+}
+
+console.log('done');
+
+// myFamily[0] // Ben
+// myFamily[1] // Jen
+// myFamily[2] // Leif
+// myFamily[3] // Oliver
