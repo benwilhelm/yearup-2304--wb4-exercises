126e1142417395c47a1f18f749326bd7f704dfdb
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-10 08:07:41 -0500

    sort courses

---
+++ Sorting/courses_sorting.js
@@ -0,0 +1,54 @@
+const courses = [
+  {
+    CourseId: 'PROG100',
+    Title: 'Introduction to HTML/CSS/Git',
+    Location: 'Classroom 7',
+    StartDate: '09/08/22',
+    Fee: '100.00'
+  },
+  {
+    CourseId: 'PROG200',
+    Title: 'Introduction to JavaScript',
+    Location: 'Classroom 9',
+    StartDate: '11/22/22',
+    Fee: '350.00'
+  },
+  {
+    CourseId: 'PROG300',
+    Title: 'Introduction to Java',
+    Location: 'Classroom 1',
+    StartDate: '01/09/23',
+    Fee: '50.00'
+  },
+  {
+    CourseId: 'PROG400',
+    Title: 'Introduction to SQL and Databases',
+    Location: 'Classroom 7',
+    StartDate: '03/16/23',
+    Fee: '50.00'
+  },
+  {
+    CourseId: 'PROJ500',
+    Title: 'Introduction to Angular',
+    Location: 'Classroom 1',
+    StartDate: '04/25/23',
+    Fee: '50.00'
+  }
+];
+
+function compareCourses(a, b) {
+  if (a.Title < b.Title) {
+    return -1;
+  }
+
+  if (b.Title < a.Title) {
+    return 1;
+  }
+  return 0;
+}
+
+courses.sort(compareCourses);
+
+for (let i = 0; i < courses.length; i++) {
+  console.log(courses[i].Title);
+}
